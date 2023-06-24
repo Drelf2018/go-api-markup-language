73c801f38c30003b71fd73728d36247725d6f189
Drelf2018 <drelf2018@outlook.com>
2023-06-24 18:50:32 +0800

    fix: 重写 lexer

--- lexer.go
+++ lexer.go
@@ -1,37 +1,41 @@
 package parser
 
-import "strings"
+import (
+	"strings"
+
+	"github.com/Drelf2020/utils"
+)
 
 const (
-	NUMBER     = iota // number
-	STRING            //string
-	LBRACKET          // [
-	RBRACKET          // ]
-	LBRACE            //  {
-	RBRACE            //  }
-	LANGLE            // <
-	RANGLE            // >
-	LGROUP            // (
-	RGROUP            // )
-	STR               // str
-	ENUM              //enum
-	TYPE              //type
-	QUERY             //query
-	BODY              //body
-	GET               //get method
-	POST              //post method
-	OPTION            //option method
-	PUT               //put method
-	DELETE            //delete method
-	HEAD              //head method
-	PATCH             //patch method
-	BOOL              // bool
-	FROM              // from
-	IMPORT            // import
-	COLON             //:
-	IDENTIFIER        // identifier
-	ASSIGNMENT        // =
-	EOF               // end
+	NUMBER     = iota + 10 // number
+	STRING                 // string
+	LBRACKET               // [
+	RBRACKET               // ]
+	LBRACE                 // {
+	RBRACE                 // }
+	LANGLE                 // <
+	RANGLE                 // >
+	LGROUP                 // (
+	RGROUP                 // )
+	NUM                    // num
+	STR                    // str
+	ENUM                   // enum
+	TYPE                   // type
+	QUERY                  // query
+	BODY                   // body
+	GET                    // get method
+	POST                   // post method
+	OPTION                 // option method
+	PUT                    // put method
+	DELETE                 // delete method
+	HEAD                   // head method
+	PATCH                  // patch method
+	BOOL                   // bool
+	FROM                   // from
+	IMPORT                 // import
+	COLON                  // :
+	IDENTIFIER             // identifier
+	ASSIGNMENT             // =
 )
 
 type Token struct {
@@ -39,227 +43,113 @@ type Token struct {
 	Value string
 }
 
-func (c *Token) New(kind int, value string) Token {
-	return Token{
-		Kind:  kind,
-		Value: value,
+func GetKind(result string) int {
+	switch result {
+	case "from":
+		return FROM
+	case "import":
+		return IMPORT
+	case "num":
+		return NUM
+	case "str":
+		return STR
+	case "enum":
+		return ENUM
+	case "type":
+		return TYPE
+	case "query":
+		return QUERY
+	case "body":
+		return BODY
+	case "bool":
+		return BOOL
+	case "GET":
+		return GET
+	case "POST":
+		return POST
+	case "OPTION":
+		return OPTION
+	case "PUT":
+		return PUT
+	case "DELETE":
+		return DELETE
+	case "HEAD":
+		return HEAD
+	case "PATCH":
+		return PATCH
+	default:
+		return IDENTIFIER
 	}
 }
 
-type Lexer struct {
-	position int64
-	text     string
-	current  rune
-}
-
-func (l *Lexer) New(text string) *Lexer {
-	s := &Lexer{
-		text:     text,
-		position: 0,
-	}
-	s.current = rune(s.text[uint64(s.position)])
-	return s
-}
-
-func (l *Lexer) PutText(text string) {
-	l.text = text
-}
-
-func (l *Lexer) advance() {
-	l.position++
-	if l.position >= int64(len(l.text)) {
-		l.current = 0
-	} else {
-		l.current = rune(l.text[uint64(l.position)])
-	}
-}
-
-func (l *Lexer) IsEOF() bool {
-	return l.current == 0
-}
-
-func (l *Lexer) Next() *Token {
-	numberic := "0123456789"
-	alphbet := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$_"
-	brackets := "(){}[]<>"
-	length := len(l.text)
-	var token *Token
-	for idx := 0; idx <= length && l.current != 0; idx++ {
-		if strings.ContainsRune(numberic, l.current) {
-			result := ""
-			for nidx := l.position; nidx < int64(len(l.text)) && strings.ContainsRune(numberic+".", l.current); nidx++ {
-				result += string(l.current)
-				l.advance()
-			}
-			token = &Token{
-				Kind:  NUMBER,
-				Value: result,
-			}
-		} else if strings.ContainsRune(brackets, l.current) {
-			switch l.current {
-			case '<':
-				token = &Token{
-					Kind:  LANGLE,
-					Value: string(l.current),
-				}
-			case '>':
-				token = &Token{
-					Kind:  RANGLE,
-					Value: string(l.current),
-				}
-			case '{':
-				token = &Token{
-					Kind:  LBRACE,
-					Value: string(l.current),
-				}
-			case '}':
-				token = &Token{
-					Kind:  RBRACE,
-					Value: string(l.current),
-				}
-			case '[':
-				token = &Token{
-					Kind:  LBRACKET,
-					Value: string(l.current),
-				}
-			case ']':
-				token = &Token{
-					Kind:  RBRACKET,
-					Value: string(l.current),
-				}
-			}
-		} else if strings.ContainsRune(alphbet, l.current) {
-			result := ""
-			for aidx := l.position; aidx < int64(len(l.text)) && strings.ContainsRune(alphbet+numberic, l.current); aidx++ {
-				result += string(l.current)
-				l.advance()
-			}
-			switch result {
-			case "from":
-				token = &Token{
-					Kind:  FROM,
-					Value: result,
-				}
-			case "import":
-				token = &Token{
-					Kind:  IMPORT,
-					Value: result,
-				}
-			case "str":
-				token = &Token{
-					Kind:  STR,
-					Value: result,
-				}
-			case "enum":
-				token = &Token{
-					Kind:  ENUM,
-					Value: result,
-				}
-			case "type":
-				token = &Token{
-					Kind:  TYPE,
-					Value: result,
-				}
-			case "query":
-				token = &Token{
-					Kind:  QUERY,
-					Value: result,
-				}
-			case "body":
-				token = &Token{
-					Kind:  BODY,
-					Value: result,
-				}
-			case "bool":
-				token = &Token{
-					Kind:  BOOL,
-					Value: result,
-				}
-			case "GET":
-				token = &Token{
-					Kind:  GET,
-					Value: result,
-				}
-			case "POST":
-				token = &Token{
-					Kind:  POST,
-					Value: result,
-				}
-			case "OPTION":
-				token = &Token{
-					Kind:  OPTION,
-					Value: result,
-				}
-			case "PUT":
-				token = &Token{
-					Kind:  PUT,
-					Value: result,
-				}
-			case "DELETE":
-				token = &Token{
-					Kind:  DELETE,
-					Value: result,
-				}
-			case "HEAD":
-				token = &Token{
-					Kind:  HEAD,
-					Value: result,
-				}
-			case "PATCH":
-				token = &Token{
-					Kind:  PATCH,
-					Value: result,
-				}
-			default:
-				token = &Token{
-					Kind:  IDENTIFIER,
-					Value: result,
-				}
-			}
-		} else if strings.ContainsRune("=", l.current) {
-			token = &Token{
-				Kind:  ASSIGNMENT,
-				Value: string(l.current),
-			}
-		} else if strings.ContainsRune(":", l.current) {
-			token = &Token{
-				Kind:  COLON,
-				Value: string(l.current),
-			}
-		} else if strings.ContainsRune(" \t", l.current) {
-			// 跳过空白字符
-			l.advance()
-			continue
-		} else if strings.ContainsRune("\"", l.current) {
-			// 字符串
-			result := ""
-			for sidx := l.position; sidx < int64(len(l.text)) && l.current != '"'; sidx++ {
-				result += string(l.current)
-				l.advance()
-			}
-			token = &Token{
-				Kind:  STRING,
-				Value: result,
-			}
-		} else {
-			if l.current == '#' {
-				// 忽略注释
-				for cidx := l.position; cidx < int64(len(l.text)) && l.current != '\n'; cidx++ {
-					l.advance()
-					continue
-				}
+func Parse(path string, ch chan Token) {
+	lexer := NewScanner(path)
+	send := func(tokens ...Token) {
+		if lexer.Length() != 0 {
+			kind := -1
+			result := lexer.Restore()
+			if utils.IsNumber(result) {
+				kind = NUMBER
 			} else {
-				token = &Token{
-					Kind:  EOF,
-					Value: "",
-				}
+				kind = GetKind(result)
 			}
+			ch <- Token{kind, result}
 		}
-		l.advance()
-		return token
+		utils.ForEach(tokens, func(t Token) { ch <- t })
 	}
-	return &Token{
-		Kind:  EOF,
-		Value: "",
+
+	for lexer.Next() {
+		s := lexer.Read()
+
+		// 多行文本
+		if lexer.HasQuotation() {
+			if lexer.First() == s {
+				// 多行字符串结束了
+				ch <- Token{STRING, lexer.Restore()[1:]}
+			} else {
+				lexer.Store()
+			}
+			continue
+		}
+
+		// 如果开头是 # 则忽略直到换行
+		if lexer.First() == "#" {
+			if s != "\n" {
+				continue
+			}
+			lexer.Restore()
+		}
+
+		// 跳过空白字符
+		if strings.Contains(" \t\n\r", s) {
+			send()
+			continue
+		}
+
+		switch s {
+		case "<":
+			send(Token{LANGLE, s})
+		case ">":
+			send(Token{RANGLE, s})
+		case "{":
+			send(Token{LBRACE, s})
+		case "}":
+			send(Token{RBRACE, s})
+		case "[":
+			send(Token{LBRACKET, s})
+		case "]":
+			send(Token{RBRACKET, s})
+		case "=":
+			send(Token{ASSIGNMENT, s})
+		case ":":
+			send(Token{COLON, s})
+		case "#":
+			send()
+			lexer.Store()
+		default:
+			lexer.Store()
+		}
 	}
+	close(ch)
+	lexer.Close()
 }
--- tests/main.go
+++ tests/main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	parser "github.com/Drelf2018/go-bilibili-api"
-	"github.com/Drelf2018/go-bilibili-api/translator"
 	"github.com/Drelf2020/utils"
 )
 
 var log = utils.GetLog()
 
 func main() {
-	am := parser.GetApi("./tests/user.aml")
-	utils.ForMap(am.Output, func(s string, a *parser.Api) { log.Info(s, " | ", *a) })
-	translator.ToPython(am, "./tests/", "user")
+	// am := parser.GetApi("./tests/user.aml")
+	// utils.ForMap(am.Output, func(s string, a *parser.Api) { log.Info(s, " | ", *a) })
+	// translator.ToPython(am, "./tests/", "user")
+	ch := make(chan parser.Token, 16)
+	go parser.Parse("./tests/user.aml", ch)
+	for t := range ch {
+		// log.Debug(t, " | ", []byte(t.Value))
+		log.Debug(t.Value)
+	}
 }
